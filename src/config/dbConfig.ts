521ecca8bcf8ed3d51a62ecce8544bca829afd89
inno-leokim <inno.leokim@gmail.com>
2024-03-27 18:00:12 +0900

    [DELETE] db conn info

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -2,11 +2,11 @@ import { Pool } from "pg";
 
 const connection = new Pool({
     user: 'yunkim',
-    host: process.env.pghost || '10.200.82.102',
+    host: process.env.pghost || '',
     database: process.env.pgdb || 'dvdrental',
-    password: process.env.pgpw || 'VMware1!',
+    password: process.env.pgpw || '',
     port: 5432,
-    max: 5
+    max: 5,
 });
 
 export default connection;
